c7aba564d451363bad420ba4d49fea86f4a7576a
Geovanne Washington <geoxp98@gmail.com>
2024-05-30 18:13:42 -0300

    new calc changes

--- calculator-project/assets/javascript/script.js
+++ calculator-project/assets/javascript/script.js
@@ -1,78 +1,66 @@
-const localScope = function() {
-    const display = document.querySelector("#display");
-    const clearButton = document.querySelector("#clear-btn");
-    const calculateButton = document.querySelector("#calculate-btn");
-    const undoButton = document.querySelector("#undo-btn");
-    const buttons = document.querySelectorAll("#keys button.normal-btn");
-
-    function appendToDisplay(inputValue) {
-        const displayValues = display.value;
-        const lastDigit = displayValues.slice(-1);
-        
+(function() {
+    const display = document.querySelector('#display');
+    
+    const buttonClick = () => {
+        addEventListener('click', (event) => {
+            const el = event.target;
+            if (el.classList.contains('btn')) appendToDisplay(el.innerText);
+            if (el.classList.contains('btn-clear')) clearDisplay();
+            if (el.classList.contains('btn-delete')) deleteLastChar();
+            if (el.classList.contains('btn-solve')) performCalc();
+        });
+    };
+    
+    const appendToDisplay = value => {
+        const lastDigit = display.value.slice(-1);
+        autoScroll();
+            
         //prevent from putting a new operator following an existing operator
         const isLastCharOperator = ['+', '-', '×', '÷'].includes(lastDigit);
-        if (isLastCharOperator && ['+', '-', '×', '÷'].includes(inputValue)) {
-            const arrayOfDigits = displayValues.slice(0, -1) + inputValue;
-            display.value = arrayOfDigits;
+        if (isLastCharOperator && ['+', '-', '×', '÷'].includes(value)) {
+            deleteLastChar();
+        }
+        display.value += value;
+    } 
+    
+    const clearDisplay = () => display.value = '';
+    const deleteLastChar = () => display.value = display.value.slice(0, -1);
+    const autoScroll = () => display.scrollLeft = display.scrollWidth;
+    
+    const performCalc = () => {
+        const result = eval(display.value.replace("÷", "/").replace("×", "*"));
+        
+        try {
+            if (!result) {
+                alert('Invalid Operation!');
+                return;
+            }
+            display.value = result;
+        } catch (error) {
+            alert('Invalid Operation!');
             return;
         }
-        display.value += inputValue;    
-    }
+    };
     
-    //removes the last digit from display
-    function undo() {
-        /*
-            This was how I first did it:
-            if (display.value === '') {
-                return;
-            }
-            const displayValues = display.value;
-            const arrayOfDigits = displayValues.split('');
-            arrayOfDigits.pop();
-            display.value = arrayOfDigits.join("");
-        */
-       
-        if (display.value !== '') {
-            const displayValues = display.value;
-            display.value = displayValues.slice(0, -1);
-        }
-    }
-    
-    function clearDisplay() {
-        //to clear the display, basically all the value is replaced by an empty string:
-        display.value = '';
-    }
-    
-    function calculate() {
-        try {
-            //checking if there is something to calculate
-            if (display.value === '') {
-                return;
-                
-            } else {
-                const displayValue = display.value;
-                //apparently it is not safe to use the eval(); function, but I will keep it in this version for now.
-                display.value = eval(displayValue.replace("÷", "/").replace("×", "*")); 
-            }
-        }
-        catch(error) {
-            display.value = "Error";
-        }
-    }
-    
-    clearButton.addEventListener("click", clearDisplay);
-    calculateButton.addEventListener("click", calculate);
-    undoButton.addEventListener("click", undo);
-
-    /*
-    Note: apparently I can do this using the forEach() function 
-    I'll implement it on a future new version of this project, once I learn more about it
-    */
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener("click", function() {
-            appendToDisplay(buttons[i].innerText);
+    pressEnter = () => {
+        addEventListener('keyup', (event) => {
+            if (event.key === 'Enter') {
+                performCalc();
+                display.focus();
+            } 
         });
-    }
-}
-
-localScope();
+    };
+    
+    pressBackspace = () => {
+        addEventListener('keydown', (event) => {
+            if (event.key === 'Backspace') {
+                clearDisplay();
+                display.focus();
+            } 
+        });
+    };
+    
+    buttonClick();
+    pressEnter();
+    pressBackspace();
+})();
